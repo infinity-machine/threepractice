0cc1d84cbb105e1f1e7f0b7807f7fdbef65e1b3f
Connor Carciofini <ccarciofini@gmail.com>
2022-11-17 14:24:35 -0600

    setup

--- client/src/components/Three.js
+++ client/src/components/Three.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import { Sky, PerspectiveCamera } from '@react-three/drei'
+import angleToRadians from '../utils/angle';
 const Three = () => {
   return (
     <>
-    <ambientLight args={['white', 1]}/>
+    <Sky/>
+    <PerspectiveCamera makeDefault position={[0, 1, 5]}/>
+    
+    <mesh position={[0, .5, 0]}>
+        <sphereGeometry args={[0.5, 32, 32]}/>
+        <meshStandardMaterial color="#ffffff"/>
+    </mesh>
+
+    <mesh rotation={[-(angleToRadians(90)), 0, 0]}>
+        <planeGeometry args={[7, 7]}/>
+        <meshStandardMaterial color="lightgreen"/>
+    </mesh>
+    <ambientLight args={["#ffffff", 1]}/>
     </>
   )
 }
---
+++ client/src/utils/angle.js
@@ -0,0 +1,6 @@
+// export const angleToRadians = (angleInDeg) => (Math.PI / 180) * angleInDeg;
+
+export default function angleToRadians(angleInDeg) {
+    const angleInRadians = ((Math.PI / 180) * angleInDeg)
+    return angleInRadians
+}
